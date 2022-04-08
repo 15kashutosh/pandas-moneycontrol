7720be1f7b49d9cc4eee6e6fe0c18c21010d27df
Ashutosh <15kashutosh@gmail.com>
2022-04-08 13:53:24 +0530

    implemented lowercase check for stock name

--- comparison.py
+++ comparison.py
@@ -5,9 +5,9 @@ print(data)
 
 #user input for checking stock price:
 def stock_input():
-    stock_name = input("Enter the Company's name: ")
+    stock_name = input("Enter the Company's name: ").lower()
     while stock_name.isalnum() ==False and ' ' not in stock_name:
-        stock_name = input("Enter the Company's name: ")
+        stock_name = input("Enter the Company's name: ").lower()
 
     stock_price = input("Enter the Company's price: ")
     while stock_price.isdigit() == False:
@@ -17,16 +17,18 @@ def stock_input():
 #stock_input comparison with the current market details:
 def stock_comparision(stock_name,stock_price):
     print(stock_name,stock_price)
-    # result = data.get(stock_name)
-    #print(result)
-    if stock_name in data.keys():
-        if ',' in data[stock_name]:
-            market_price = float(data[stock_name].replace(',',''))
+
+    processed_data = {}
+    for item in data.keys():
+        processed_data[item.lower()] = data[item]
+    if stock_name in processed_data.keys():
+        if ',' in processed_data[stock_name]:
+            market_price = float(processed_data[stock_name].replace(',',''))
         else:
-            market_price = float(data[stock_name])
+            market_price = float(processed_data[stock_name])
         if stock_price>market_price:
             print(f"Time to buy {stock_name}")
-            print(f"{stock_name}'s  Last Traded Price : {data[stock_name]}")
+            print(f"{stock_name}'s  Last Traded Price : {processed_data[stock_name]}")
         else:
             print("Price criteria not satisfied")
     else:
